5a5c5f89b53e1eecccf19fedfaec1a72007181c2
SnehaNirale <snehanirale125@gmail.com>
2020-11-12 12:27:35 +0530

    fist change in devolop branch

--- src/test/java/Academy/AppTest.java
+++ src/test/java/Academy/AppTest.java
@@ -21,4 +21,18 @@ public class AppTest
         System.out.println("How r u..........");
         System.out.println("I am fine..........");
     }
+    
+    public void shouldAnswerWithTrue_a()
+    {
+        assertTrue( true );
+        System.out.println("Hi..........");
+        System.out.println("Hello..........");
+        System.out.println("How r u..........");
+        System.out.println("Sneha..........");
+    }
+    
+    public void Demo()
+    {
+    	System.out.println("Being Human..........");
+    }
 }
